2bd0db46fa4ffeac387419584dc68cba20290f0c
José Pacheco <43217967+checopa@users.noreply.github.com>
2018-10-11 10:13:50 -0300

    Add files via upload

---
+++ Tarea1/P1Discretas.py
@@ -0,0 +1,92 @@
+# Run the file as "python SAT.py -v"
+
+# Add further needed modules
+import unittest
+
+# To implement the functions below, you are allowed
+# to define auxiliary functions, if convenient.
+
+
+def SetLiteral(formula, lit):
+    newlit = []
+    for i in formula:
+        litlit = []
+        for j in i:
+            if j == lit:
+                litlit = []
+                break
+            if j == -lit:
+                continue
+            litlit.append(j)
+        if len(litlit) != 0:
+            newlit.append(litlit)
+    return newlit
+
+
+def IsSatisfiable(formula):
+    if len(formula) == 0:
+        return True
+    if len(formula) > 1:
+        c = 0
+        for i in formula:
+            if len(i) == 1 and c == 0:
+                c = i[0]
+            if len(i) == 1 and i[0] == -c:
+                return False
+    lit = formula[0][0]
+    for i in formula:
+        if len(i) == 1:
+            lit = i[0]
+            break
+    return IsSatisfiable(SetLiteral(formula, lit))
+
+
+
+
+def BuildModel(formula):
+    if not IsSatisfiable(formula):
+        return (False, {})
+    else:
+        literales = []
+        while len(formula) >= 1:
+            lit = formula[0][0]
+            for i in formula:
+                if len(i) == 1:
+                    lit = i[0]
+            if IsSatisfiable(SetLiteral(formula, lit)):
+                literales.append([lit, True])
+                formula = SetLiteral(formula, lit)
+            else:
+                literales.append([-lit, True])
+                formula = SetLiteral(formula, -lit)
+        for i in literales:
+            if i[0] < 0:
+                i[0] = -i[0]
+                i[1] = not i[1]
+        literales.sort()
+        newliterales={}
+        for i in literales:
+            newliterales[i[0]]=i[1]
+        return (True, newliterales)
+
+
+class Tests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_SetLiteral(self):
+        self.assertEqual(SetLiteral([[1, 2, -3], [-1, -2, 4], [3, 4]], 1), [[-2, 4], [3, 4]])
+        self.assertEqual(SetLiteral([[1, 2, -3], [-1, -2, 4], [3, 4]], -1), [[2, -3], [3, 4]])
+
+    def test_IsSatisfiable(self):
+        self.assertEqual(IsSatisfiable([[1, 2, -3], [-1, -2, 4], [3, 4]]), True)
+        self.assertEqual(IsSatisfiable([[1, 2], [1, -2], [], [-1]]), False)
+        self.assertEqual(IsSatisfiable([]), True)
+
+    def test_BuildModel(self):
+        self.assertEqual(BuildModel([[-2, 4], [1], [-4,-1]]), (True, {1: True, 2: False, 4: False}))
+        self.assertEqual(BuildModel([[1,2], [-1,-2], [-1,2], [1,-2]]), (False, {}))
+
+# Perform the tests when runing the file
+if __name__ == '__main__':
+    unittest.main()
